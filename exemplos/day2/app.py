b3a95c1aa455e2ad5f81ad03007c0d8fd2e64e49
vsantsal <vsantsal@gmail.com>
2025-04-26 22:28:15 -0300

    feature: acompanhando video 3 contextos

--- exemplos/day2/app.py
+++ exemplos/day2/app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, url_for
 
 
 app = Flask(__name__)
 
+app.config['APP_NAME'] = 'Meu Blog'
+
 @app.route("/")
-def hello():
+def index():
     """Ponto de entrada da aplicação"""
-    return "<strong>Hello world</strong>"
+    content_url = url_for("read_content", title="Novidades de 2025")
+    return (
+        f"<h1>{app.config['APP_NAME']}</h1>"
+        f"<a href='{content_url}'>Novidades de 2025</a>"
+        "<hr>"
+    )
+
+
+@app.errorhandler(404)
+def not_found_page(error):
+    """Página de erro 404"""
+    return f"<strong>Page not found on {app.config['APP_NAME']}</strong>", 404
+
+
+def read_content(title):
+    """Lê o conteúdo de um artigo"""
+    index_url = url_for("index")
+    return f"<h1>{title}</h1> <a href='{index_url}'>Voltar</a>"
+
+
+app.add_url_rule("/<title>", view_func=read_content)
