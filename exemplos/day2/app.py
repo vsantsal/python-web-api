b00d26f7d9b291c1c0be689d5e43d42877b02c71
vsantsal <vsantsal@gmail.com>
2025-04-27 21:06:12 -0300

    feature: acompanhando video extensao para nosql com mongodb

--- exemplos/day2/app.py
+++ exemplos/day2/app.py
@@ -1,18 +1,27 @@
 from flask import Flask, url_for
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
 
 app.config['APP_NAME'] = 'Meu Blog'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/blog"
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
 def index():
     """Ponto de entrada da aplicação"""
+
+    posts = mongo.db.posts.find() # generator
+
     content_url = url_for("read_content", title="Novidades de 2025")
     return (
         f"<h1>{app.config['APP_NAME']}</h1>"
         f"<a href='{content_url}'>Novidades de 2025</a>"
         "<hr>"
+        f"{list(posts)}"
     )
 
 
